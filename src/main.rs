65cd75e290566ec763dbe921565369e65319d372
four0000four <tom@fourzerofour.pw>
2020-05-26 22:51:35 +1000

    Hardcoded gist uploaded w/ dummy token
    CHANGELOG
    
    This is all fresh, so here's what's important:
    
    We have structs defining JSON request and response payloads, which
    are de/serialized by serde. This should hold for now.
    
    We can successfully read a file into a string. Let's break this out
    into its own function for usability.
    
    We can connect file reads into the expected format for gist uploads.
    Let's break this out too, so we can add a --dry-run feature later.
    
    We can POST this using a token to the right URL. Probably leave this
    for now while we figure out the features that we need -- i.e.: how
    can a gist be uploaded programmatically?
    
    TODO
    
    1. Read up on all the stuff we should care about re: Gists
    2. From this, develop some key UX -- what goes in, what comes out?
    3. Come up with some example workflows, e.g.:
    
    $ fd test.sh | xargs cat | gst create --public
    ... realtime log information showing upload status ...
    
    $ gst list --user junegunn
    ... list of \t-separated $ID $NAME $DESCRIPTION ...
    extra options:
     --since (iso-8601 timestamp)
     --show-created-at (iso-8601)
     --show-updated-at (iso-8601)
     --show-public-status (for client-side filtering, usage)
     --public-only (gets server to filter out private gists)
     --count (over 3000 prompts additional downloads via pagination)
    
    ... and so on. More at:
    https://developer.github.com/v3/gists/#list-gists-for-a-user
    
    FUTURE WORK
    
    Once some basic functionality's working, let's stick it on GitHub
    as an alpha release and explicitly say "hey, you need to supply
    your own personal token and compile from source".
    
    Then set up CI/CD, and specify that a specific token needs to
    exist at "~/.gst"
    
    Then try and get it on the rust-awesome page, HN, and Reddit.
    
    Then finally hook it up for OAuth and write a blog post about it.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,57 @@
-use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Debug)]
+struct CreateGistFilePayload {
+    content: String,
+}
+
+#[derive(Serialize, Debug)]
+struct CreateGistPayload {
+    description: String,
+    public: bool,
+    files: std::collections::HashMap<String, CreateGistFilePayload>,
+}
+
+#[derive(Deserialize, Debug)]
+struct CreateGistResponsePayload {
+    url: String,
+    html_url: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+    let mut file = File::open("test.gist")?;
+    let mut gist_data = String::new();
+    file.read_to_string(&mut gist_data)?;
+
+    let mut files = std::collections::HashMap::new();
+    files.insert(
+        String::from("test.gist"),
+        CreateGistFilePayload { content: gist_data },
+    );
+
+    let payload = CreateGistPayload {
+        description: String::from("test gist"),
+        public: true,
+        files: files,
+    };
+
+    let url: &str = "https://api.github.com/gists";
+    let client = reqwest::Client::new();
+    let token = "56122920292a664576ebd5ded0e381ba88dc7ea0";
+    let req = client
+        .post(url)
+        .json(&payload)
+        .header("user-agent", "gst")
+        .header("authorization", format!("token {}", token));
+
+    let resp = req.send().await?;
+
+    println!("{:?}", resp.status());
+    println!("{:?}", resp.json::<CreateGistResponsePayload>().await?);
+
     Ok(())
 }
