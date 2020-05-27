5193071202812ee85763352c9b0c4689d1ce2357
four0000four <tom@fourzerofour.pw>
2020-05-27 18:23:09 +1000

    Comments added to stub future refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -25,21 +25,23 @@ pub async fn create(
     is_public: bool,
     description: String,
 ) -> Result<CreateGistResponsePayload, Box<dyn std::error::Error>> {
+    // filepaths: Vec<String> => textContentsByFilepath: HashMap<String, String>
     let mut payload_map = std::collections::HashMap::new();
     for fp in files {
         let mut file = std::fs::File::open(&fp)?;
         let mut gist_data = String::new();
         file.read_to_string(&mut gist_data)?;
-
         payload_map.insert(fp, CreateGistFilePayload { content: gist_data });
     }
 
+    // Set up JSON payload for create operation
     let payload = CreateGistPayload {
         description,
         public: is_public,
         files: payload_map,
     };
 
+    // POST authenticated create request
     let url: &str = "https://api.github.com/gists";
     let client = reqwest::Client::new();
     let token = "56122920292a664576ebd5ded0e381ba88dc7ea0";
@@ -49,7 +51,12 @@ pub async fn create(
         .header("user-agent", "gst")
         .header("authorization", format!("token {}", token));
 
+    // Await API response
     let resp = req.send().await?;
+
+    // Parse API response
     let json = resp.json::<CreateGistResponsePayload>().await?;
+
+    // Return parsed response, as struct
     Ok(json)
 }
